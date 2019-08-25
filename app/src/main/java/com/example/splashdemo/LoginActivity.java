35be3272b352935375c7aca3fd247fd8c5d1f2f0
jkangogo <46781298+jkangogo@users.noreply.github.com>
2019-08-25 19:14:45 +0300

    welcome splash_demo

---
+++ app/src/main/java/com/example/splashdemo/LoginActivity.java
@@ -0,0 +1,23 @@
+package com.example.splashdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+    EditText username,password;
+    Button login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        username = findViewById(R.id.etUsername);
+        password = findViewById(R.id.etpassword);
+        login = findViewById(R.id.Login);
+    }
+}
